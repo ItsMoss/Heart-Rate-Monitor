dadc6aaa94f18086f744e7afbc584ae3aea9fe24
mmj9@duke.edu <mmj9@duke.edu>
2016-09-24 19:39:52 -0400

    a bit of reorganizing files

--- heart_rate.py
+++ heart_rate.py
@@ -1,32 +1,3 @@
-def main(binary_file="test.bin",time=5, n=2):
-    """
-    This is the main file that runs the whole program
-    
-    :param int time: duration of time (in seconds) being read-in from binary file
-    :param int n: number of signals being multiplexed
-    """    
-    
-    b = 0 # counter for the byte number that is being read in
-    
-    Fs, b = read_data(binary_file, b) # determine sampling frequency
-    
-    # make sure time is at least 5 seconds and a whole number
-    if time < 5:
-        time = 5
-    time //= 1
-    
-    samples = Fs * time
-    
-    signals = [[0 for x in range(samples)] for x in range(n)]
-    
-    for i in range(samples):
-        for j in range(len(signals)):
-
-            v, b = read_data(binary_file, b)
-            signals[j][i] = v
-        
-            print("signal %d: " %(j+1), signals[j]) # NOTE. This is for testing
-
 def read_data(file, read_from):
     """
     This function reads in a single byte from a binary file and converts it to
@@ -43,15 +14,3 @@ def read_data(file, read_from):
         v = int.from_bytes(bs, 'little')
         
     return v, read_from + 2
-    
-def check_data(signals):
-    """
-    This function ensures that all values read in from the input signal are
-    valid (i.e. not NaN) in order to do arithmetic on
-    
-    :param list signals: contains the read-in signal(s)
-    """
-    pass
-
-if __name__ == '__main__':
-    main()
---
+++ heart_rate_main.py
@@ -0,0 +1,34 @@
+import heart_rate as hr
+
+def main(binary_file="test.bin",time=5, n=2):
+    """
+    This is the main file that runs the whole program
+    
+    :param int time: duration of time (in seconds) being read-in from binary file
+    :param int n: number of signals being multiplexed
+    """    
+    
+    b = 0 # counter for the byte number that is being read in
+    
+    Fs, b = hr.read_data(binary_file, b) # determine sampling frequency
+    
+    # make sure time is at least 5 seconds and a whole number
+    if time < 5:
+        time = 5
+    time //= 1
+    
+    samples = Fs * time
+    
+    signals = [[0 for x in range(samples)] for x in range(n)]
+    
+    for i in range(samples):
+        for j in range(len(signals)):
+
+            v, b = hr.read_data(binary_file, b)
+            signals[j][i] = v
+        
+            print("signal %d: " %(j+1), signals[j]) # NOTE. This is for testing
+            
+
+if __name__ == '__main__':
+    main()
